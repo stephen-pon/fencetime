9e1bd3e471dc0dfa49eed085478c2825d6f06f0c
Stephen Pon <stephen@stephen.stephen>
2014-02-16 21:25:15 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fencetime::Application.config.secret_key_base = '1b1dea127e99cd52f83284bef090447858ad4d6b1280946da8921a6d08512263c321fb2c401c9d9dad25e81f92fab63bffbe40ab6cb53fbb80062ca7f7e4bb26'
